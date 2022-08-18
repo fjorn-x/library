50b4c63bba676f71dada9f5a7f352e25a43a27d3
fjorn-x <hamzashaikh00@gmail.com>
2022-08-18 23:49:42 +0530

    Minor Updates to the cards

--- app.js
+++ app.js
@@ -47,16 +47,22 @@ function getBookfromInput() {
     ? "Read"
     : "Not Read";
 
-  return new Book(title, author, pages, isRead);
+  let dateAdded = new Date().toLocaleDateString();
+
+  return new Book(title, author, pages, isRead, dateAdded);
 }
+console.log(typeof new Date().toLocaleDateString());
 
-const myLibrary = [];
+const myLibrary = [
+  // { title: "zuzu", author: "mum", pages: 12, read: "Read" }
+];
 
-function Book(title = "Unknown", author = "Unknown", pages = 0, read = false) {
+function Book(title, author, pages, read, date) {
   this.title = title;
   this.author = author;
   this.pages = pages;
   this.read = read;
+  this.date = date;
 }
 
 function addBookToLibrary(book) {
@@ -65,36 +71,50 @@ function addBookToLibrary(book) {
 
 function display(bookCounter) {
   const obj = myLibrary[bookCounter];
-  const newCard = document.createElement("div");
 
+  const newCard = document.createElement("div");
   newCard.classList.add("card");
-  newCard.innerHTML = ` <div class="card">
-  <div class="card__side card__side--back">
-    <div class="card__cover">
-      <h4 class="card__heading">
-        <span class="card__heading-span">Author</span>
-      </h4>
+  newCard.innerHTML = `
+  <div class="card_side card_side--back">
+    <div class="card_cover">
+      <h1 >
+        ${obj.author}
+      </h1>
     </div>
-    <div class="card__details"></div>
+    <div class="card_details">
+      <div class="pages">
+      <h2>Pages</h2>
+        <h3>${obj.pages}</h3>
+      </div>
+      
+      <div class="date-added">
+      <h2>Date Added</h2>
+      <h3>${obj.date}</h3>
+      </div>
+      
+      <div class="is-read">
+      <h2>Status</h2>
+        <button class="read-status" type="button">${obj.read}</button>
+      </div>
+      <div class="remove">
+      <h2>Remove</h2>
+      <img src="./images/delete-icon.png">
+      </div>
+
+    </div>
   </div>
 
-  <div class="card__side card__side--front">
-    <div class="card__theme">
-      <div class="card__theme-box">
-        <p class="card__title">Title</p>
+  <div class="card_side card_side--front">
+    <div class="card_theme">
+      <div class="card_theme-box">
+        <h1 class="card_title">${obj.title}</h1>
       </div>
     </div>
   </div>
 </div>
 `;
-  // const titleDisplay = document.createElement("div");
-  // titleDisplay.classList.add("title-display");
-  // titleDisplay.textContent = obj.title;
-  // newCard.appendChild(titleDisplay);
-  // // for (const key in obj) {
-  // //   const properties = document.createElement("h1");
-  // //   properties.textContent = obj[key];
-  // //   newCard.appendChild(properties);
-  // // }
+
   booksContainer.appendChild(newCard);
 }
+
+display(0);
